09a5df45a63a20744400dcce6eb5152125be367c
Seb-mlrd <sebmlrd06@gmail.com>
2025-02-04 14:51:13 +0100

    ajout du signin

--- app/sign/page.jsx
+++ app/sign/page.jsx
@@ -1,10 +1,10 @@
 import SignUp from '../components/SignUp'; // Composant d'inscription
 import SignIn from '../components/SignIn'; // Composant de connexion
-import style from './page.module.css';
+import styles from './page.module.css';
 
 export default function SignPage() {
   return (
-    <div style={style.container}>
+    <div className={styles.container}>
       <SignIn />
     </div>
   )
