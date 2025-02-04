b1890e665d7f22a74c775502a27047314a05a2c0
Yvan Kerdanet <yvan.kerdanet@gmail.com>
2025-02-04 14:48:13 +0100

    Refactor SignUp component to include nickname field and switch to SignIn, add styling for sign page layout, and include react-icons dependency

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 // import { Link } from 'react-router-dom'
 import axios from 'axios'
 // import Header from '../components/Header';
-const SignUp = () => {
-    const [user, setUser] = useState();
+export default function SignUp ({ setSigningUp, setSigningIn }) {
+    const [user, setUser] = useState({});
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -19,50 +19,55 @@ const SignUp = () => {
             console.log(e.message);
         }
     };
-  return (
-    <div className="register-container">
-      <h1 className="register-title">CaféTalk - Inscription</h1>
-      <div className="register-content">
-          <form onSubmit={handleSubmit} className="register-form">
-              <label htmlFor="prenom" className="register-label">Prénom :</label>
-              <input
-                  type="text"
-                  onChange={handleChange}
-                  placeholder="Entrez votre prénom"
-                  name="prenom"
-                  className="register-input"
-                   
-              />
-              <label htmlFor="avatar" className="register-label">Avatar :</label>
-              <input
-                  type="file"
-                  onChange={handleChange}
-                  name="avatar"
-                  className="register-input"
-              />
-              <label htmlFor="email" className="register-label">Email :</label>
-              <input
-                  type="email"
-                  onChange={handleChange}
-                  placeholder="Entrez votre email"
-                  name="email"
-                  className="register-input"
-                   
-              />
-              <label htmlFor="password" className="register-label">Mot de passe :</label>
-              <input
-                  type="password"
-                  onChange={handleChange}
-                  placeholder="Votre mot de passe"
-                  name="password"
-                  className="register-input"
-                   
-              />
-              <button type="submit" className="register-button">Rejoindre le café</button>
-          </form>
-      </div>
-  </div>
-  )
-}
 
-export default SignUp
+    const switchToSignIn = () => {
+        setSigningUp(false);
+        setSigningIn(true);
+    };
+
+    return (
+        <div className="register-container">
+            <h1 className="register-title">CaféTalk - Inscription</h1>
+            <div className="register-content">
+                <form onSubmit={handleSubmit} className="register-form">
+                    <label htmlFor="prenom" className="register-label">Pseudo :</label>
+                    <input
+                        type="text"
+                        onChange={handleChange}
+                        placeholder="Entrez votre pseudo"
+                        name="nickname"
+                        className="register-input"
+                        required
+                    />
+                    <label htmlFor="avatar" className="register-label">Avatar :</label>
+                    <input
+                        type="file"
+                        onChange={handleChange}
+                        name="avatar"
+                        className="register-input"
+                    />
+                    <label htmlFor="email" className="register-label">Email :</label>
+                    <input
+                        type="email"
+                        onChange={handleChange}
+                        placeholder="Entrez votre email"
+                        name="email"
+                        className="register-input"
+                        required
+                    />
+                    <label htmlFor="password" className="register-label">Mot de passe :</label>
+                    <input
+                        type="password"
+                        onChange={handleChange}
+                        placeholder="Votre mot de passe"
+                        name="password"
+                        className="register-input"
+                        required
+                    />
+                    <button type="submit" className="register-button">Rejoindre le café</button>
+                </form>
+            </div>
+            <button onClick={switchToSignIn} className="register-switch">Déjà inscrit ? Connectez-vous</button>
+        </div>
+    )
+};
--- app/sign/page.jsx
+++ app/sign/page.jsx
@@ -1,11 +1,23 @@
+"use client";
 import SignUp from '../components/SignUp'; // Composant d'inscription
 import SignIn from '../components/SignIn'; // Composant de connexion
 import style from './page.module.css';
+import { DiCoffeescript } from "react-icons/di";
+import React, { useState } from 'react';
 
 export default function SignPage() {
+  const [signingUp, setSigningUp] = useState(true);
+  const [signingIn, setSigningIn] = useState(false);
+
   return (
-    <div style={style.container}>
-      Page d'inscription
+    <div className={style.container}>
+      <div className={style.containerIcon}>
+        <DiCoffeescript size={500} />
+      </div>
+      <div className={style.containerForm}>
+        {signingUp && <SignUp />}
+        {signingIn && <SignIn />}
+      </div>
     </div>
   )
 }
