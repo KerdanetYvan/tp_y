c4df2a452e5f582370765a3b0ed007b2855b6ee7
Charly <charlyplanquettepro@gmail.com>
2025-02-04 16:05:41 +0100

    change

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,10 @@ import style from "./page.module.css";
 export default function Home() {
   return (
     <div>
-        <h1>Page d'accueil</h1>
       <div className={styles.container_content}>
-        <aside className={styles.aside_left}>gauche</aside>
-        <main className={styles.main}>milieu</main>
-        <aside className={styles.aside_right}>droite</aside>
+        <aside className={styles.aside_left}></aside>
+        <main className={styles.main}></main>
+        <aside className={styles.aside_right}></aside>
       </div>
     </div>
   );
